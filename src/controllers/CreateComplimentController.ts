3d612ad4caaec4aba91c24faa6b2654df867b6b3
Jessica Castro <jessica.castros@outlook.com>
2021-06-26 20:08:18 -0300

    Fix on ComplimentController and on ensure authenticated file name

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -3,11 +3,17 @@ import { CreateComplimentService } from "../services/CreateComplimentService";
 
 class CreateComplimentController {
   async handle(request: Request, response: Response) {
-    const { tag_id, user_sender, user_receiver, message } = request.body;
+    const { tag_id, user_receiver, message } = request.body;
+    const { user_id } = request;
     
     const createComplimentService = new CreateComplimentService();
 
-    const compliment = await createComplimentService.execute({ tag_id, user_sender, user_receiver, message });
+    const compliment = await createComplimentService.execute({ 
+      tag_id, 
+      user_sender: user_id, 
+      user_receiver, 
+      message 
+    });
 
     return response.json(compliment);
   }
